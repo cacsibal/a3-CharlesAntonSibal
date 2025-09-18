2f25232128bc8f31186a39ab3e9a7011eef0399d
cacsibal <cacsibal@gmail.com>
2025-09-17 22:16:59 -0400

    started work on viewing categories for task creation

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,25 @@
-window.onload = function () {
-    fetch('/api/categories')
+const categories = []
+
+const populateCategorySelect = async function () {
+    await fetch('/api/categories')
         .then(res => res.json())
-        .then(categories => {
-            console.log(categories);
+        .then(resCategories => {
+            for (const category of resCategories) {
+                categories.push(category.category)
+            }
+            console.log(categories)
         })
         .catch(err => console.error(err));
+
+    const categorySelect = document.getElementById('select-category');
+    for (const category of categories) {
+        const option = document.createElement('option');
+        option.value = category;
+        option.text = category;
+        categorySelect.appendChild(option);
+    }
+}
+
+window.onload = function () {
+    populateCategorySelect();
 }
